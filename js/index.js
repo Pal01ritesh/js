13a8a69b637a4c5e4840931f7d11c44dfc2c20e8
Pal01ritesh <114793885+Pal01ritesh@users.noreply.github.com>
2022-12-08 08:30:21 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// same as print statement in JS
+ console.log('hello world')   
+
+
+//  creating a variable in js
+ let a = 1
+let message = 'Hello there my name is Ritesh Pal'
+
+// creating variables in a single line
+let num = 100, mes = 'Hello there my name is Ritesh Pal'
+
+//automatically adds a semicolon
+let n = 10;
+console.log(n);
+let z = 100;
+console.log(z);
+
+
+//creating a pop up alert in the browser
+
+alert('hello world')
+
+
+// creating alert and asking for a input
+let input = prompt('Enter a number')
+console.log(input)
+
+
+let isProgrammer = confirm('Are you a programmer')
+
+console.log(isProgrammer)
+
+
+
+// commenting in js
+
+
+//single line comment
+
+/*let
+a =
+10 multiline comment */
+
+// 1.common datatypes 
+// 2.String => text
+// 3.Numbers => int as well as float
+// 4.Arrays => ordered pair of collection [10,20,30]
+// 5.Objects => key value pair {name: 'John'}
+// 6.boolean => true and false
+
+// {} => a block
+//Conditional statements
+if (true) {
+    alert('hello')
+}
+alert('world')
+
+
+
+
+let age = prompt('How old are you')
+
+if (age < 18) {
+    alert('you are not allowed to cast vote ')
+} else if (age >= 18) {
+    alert('you are allowed to cas votee')
+}
