712ed77a070e838c08a97084ded5dd2627f70815
Pal01ritesh <114793885+Pal01ritesh@users.noreply.github.com>
2022-12-09 08:34:34 +0530

    Add files via upload

---
+++ js/conditional_example.js
@@ -0,0 +1,11 @@
+let age = 18
+
+if (age >= 18) {
+    console.log('You are allowed to cast vote')
+} else if (age < 2018) {
+    console.log('You are not allowed to cast vote')
+
+} else {
+    console.log('please input valid age')
+
+}
